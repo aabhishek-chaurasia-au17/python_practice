7f1da372e5fd0aed0a6e6da20ec79b3f59749817
abhishek <abhishekc838@gmail.com>
2021-04-01 16:35:23 +0530

    new changes

---
+++ loop_nested.py
@@ -0,0 +1,7 @@
+
+
+for i in range(10):
+     cnt = 0
+     while cnt < 10:
+         print(cnt)
+         cnt += 1
--- practice_3.py
+++ practice_3.py
@@ -2,12 +2,18 @@
 print("enter your age")
 user_abhi = int(input())
 
-if(user_abhi > 18):
-    print("driver bn skte ho")
 
-    if(user_abhi < 18):
-        print("abhi tum bachhe ho beta")
+
+if(user_abhi >= 18):
 
     if(user_abhi > 40):
         print("tum to bade havey driver ho")
+    else:
+        print("driver bn skte ho")
+
+
+elif(user_abhi < 18):
+    print("abhi tum bachhe ho beta")
+
+
 
