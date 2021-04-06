dc79ead0d010e2b3ce510c4c768c9bb836264aa7
abhishek <abhishekc838@gmail.com>
2021-04-06 17:37:58 +0530

    list practice

---
+++ list/start.py
@@ -0,0 +1,20 @@
+
+# names = ["abhishek", "amit", "adil","anil","pallav"]
+
+
+# for name in names:
+#     print("hello",name)
+
+
+fruits = ["orenge", "mango", "banana"]
+
+
+
+fruits.append("kivi")
+fruits.append("cherry")
+fruits.append("greps")
+
+fruits.insert(3,"apple")
+
+fruits.pop()
+print(fruits)
